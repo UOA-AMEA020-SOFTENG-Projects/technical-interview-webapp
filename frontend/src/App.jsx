b8b185797dd933eb00e06291ba39cdc4efb606f1
IbrahimAnees <iane056@aucklanduni.ac.nz>
2023-05-27 22:54:02 +1200

    Connected frontend with backend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import CodeEditor from './CodeEditor';
+import './App.css'
+
+// npm install react-ace ace-builds brace
+const App = () => {
+
+  return (
+    <>  
+      <h1>This is a simple code editor</h1>
+      <CodeEditor />
+    </>
+  )
+}
+
+export default App;
---
+++ frontend/src/CodeEditor.jsx
@@ -0,0 +1,82 @@
+import { useState } from "react";
+import AceEditor from "react-ace";
+
+import "ace-builds/src-noconflict/mode-javascript";
+import "ace-builds/src-noconflict/mode-java";
+import "ace-builds/src-noconflict/theme-monokai";
+
+function CodeEditor() {
+  const [value, setValue] = useState("");
+
+  const userInputHandler = (newValue) => {
+    setValue(newValue);
+  };
+
+  const submitHandler = () => {
+    const codeData = {
+      run_spec: {
+        language_id: "python3",
+        sourcefilename: "HelloWorld.py",
+        sourcecode: value,
+      },
+    };
+    submitCode(codeData);
+  };
+
+  const submitCode = async (code) => {
+    const textResponse = await fetch(
+      "http://localhost:4000/jobe/index.php/restapi/runs",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(code),
+      }
+    );
+
+    if (!textResponse.ok) {
+      throw json(
+        {
+          message:
+            "Could not successfully run code.",
+        },
+        { status: 500 }
+      );
+    }
+
+    const output = await textResponse.json();
+    console.log(output);
+  };
+
+  return (
+    <>
+      <div>
+        <AceEditor
+          mode="java"
+          theme="monokai"
+          onChange={userInputHandler}
+          name="editor"
+          editorProps={{ $blockScrolling: true }}
+          value={value}
+          fontSize={14}
+          showPrintMargin={true}
+          showGutter={true}
+          highlightActiveLine={true}
+          setOptions={{
+            enableBasicAutocompletion: true,
+            enableLiveAutocompletion: true,
+            enableSnippets: true,
+            showLineNumbers: true,
+            tabSize: 2,
+          }}
+        />
+      </div>
+      <div>
+        <button onClick={submitHandler}>Run</button>
+      </div>
+    </>
+  );
+}
+
+export default CodeEditor;
