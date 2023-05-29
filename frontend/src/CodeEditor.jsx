5c3149ee0010b416673da28f5bd5a76977f2b112
jsan956-ai <jsan956@aucklanduni.ac.nz>
2023-05-30 02:15:30 +1200

    added filter which allows the user to select the language

--- frontend/src/CodeEditor.jsx
+++ frontend/src/CodeEditor.jsx
@@ -4,20 +4,56 @@ import AceEditor from "react-ace";
 import "ace-builds/src-noconflict/mode-javascript";
 import "ace-builds/src-noconflict/mode-java";
 import "ace-builds/src-noconflict/theme-monokai";
+import "ace-builds/src-noconflict/ext-language_tools"; 
+import "ace-builds/src-noconflict/snippets/java"; 
+
 
 function CodeEditor() {
   const [value, setValue] = useState("");
   const [output, setOutput] = useState("");
+  const [selectedLanguage, setSelectedLanguage] = useState("java");
+  const [fileName, setFileName] = useState("javaOutput.java");
 
   const userInputHandler = (newValue) => {
     setValue(newValue);
   };
 
+  const dropDownChangeHandler = (event) => {
+    setSelectedLanguage(event.target.value);
+    
+    // set the output filename and ext based on the language chosen 
+    switch (event.target.value) {
+      case 'c':
+        setFileName(event.target.value + "-output.c");
+        break;
+      case 'java':
+        setFileName(event.target.value + "Output.java");
+        break;
+      case 'nodejs':
+        setFileName(event.target.value + "-output.js");
+        break;
+      case 'python3':
+        setFileName(event.target.value + "-output.py");
+        break;
+      case 'octave':
+        setFileName(event.target.value + "-output.m");
+        break;
+      case 'cpp':
+        setFileName(event.target.value + "-output.cpp");
+        break;
+      case 'php':
+        setFileName(event.target.value + "-output.php");
+        break;
+      default:
+        console.log("Please select a valid language");
+    }
+  }
+
   const submitHandler = () => {
     const codeData = {
       run_spec: {
-        language_id: "python3",
-        sourcefilename: "HelloWorld.py",
+        language_id: selectedLanguage,
+        sourcefilename: fileName,
         sourcecode: value,
       },
     };
@@ -37,13 +73,7 @@ function CodeEditor() {
     );
 
     if (!textResponse.ok) {
-      throw json(
-        {
-          message:
-            "Could not successfully run code.",
-        },
-        { status: 500 }
-      );
+      console.log("status: " + textResponse.status);
     }
 
     const output = await textResponse.json();
@@ -53,6 +83,17 @@ function CodeEditor() {
 
   return (
     <>
+      <div>
+        <select value={selectedLanguage} default={selectedLanguage} onChange={dropDownChangeHandler}>
+            <option value='c'>C</option>
+            <option value='java'>Java</option>
+            <option value='nodejs'>NodeJS</option>
+            <option value='python3'>Python</option>
+            <option value='octave'>Octave</option>
+            <option value='cpp'>C++</option>
+            <option value='pascal'>Pascal</option>
+        </select>
+      </div>
       <div>
         <AceEditor
           mode="java"
@@ -66,11 +107,11 @@ function CodeEditor() {
           showGutter={true}
           highlightActiveLine={true}
           setOptions={{
-            enableBasicAutocompletion: true,
-            enableLiveAutocompletion: true,
-            enableSnippets: true,
-            showLineNumbers: true,
-            tabSize: 2,
+            enableBasicAutocompletion: true, 
+            enableLiveAutocompletion: true, 
+            enableSnippets: true, 
+            showLineNumbers: true, 
+            tabSize: 2, 
           }}
         />
       </div>
