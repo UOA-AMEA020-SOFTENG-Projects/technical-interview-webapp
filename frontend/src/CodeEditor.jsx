ca68f1fa6a71689eae14eee4de31f9b6b3bb280a
jsan956-ai <jsan956@aucklanduni.ac.nz>
2023-05-30 03:13:31 +1200

    error checking implemented

--- frontend/src/CodeEditor.jsx
+++ frontend/src/CodeEditor.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import AceEditor from "react-ace";
 
 import "ace-builds/src-noconflict/mode-javascript";
@@ -7,12 +7,17 @@ import "ace-builds/src-noconflict/theme-monokai";
 import "ace-builds/src-noconflict/ext-language_tools"; 
 import "ace-builds/src-noconflict/snippets/java"; 
 
+import "ace-builds/src-noconflict/mode-c_cpp";  
+import "ace-builds/src-noconflict/mode-python";  
+import "ace-builds/src-noconflict/mode-php";
 
 function CodeEditor() {
   const [value, setValue] = useState("");
   const [output, setOutput] = useState("");
   const [selectedLanguage, setSelectedLanguage] = useState("java");
   const [fileName, setFileName] = useState("javaOutput.java");
+  const [errorMsg, setErrorMsg] = useState("");
+  const [isErrorVisible, setIsErrorVisible] = useState(false);
 
   const userInputHandler = (newValue) => {
     setValue(newValue);
@@ -34,9 +39,6 @@ function CodeEditor() {
         break;
       case 'python3':
         setFileName(event.target.value + "-output.py");
-        break;
-      case 'octave':
-        setFileName(event.target.value + "-output.m");
         break;
       case 'cpp':
         setFileName(event.target.value + "-output.cpp");
@@ -74,22 +76,41 @@ function CodeEditor() {
 
     if (!textResponse.ok) {
       console.log("status: " + textResponse.status);
+      setErrorMsg("Something went wrong.");
+      setIsErrorVisible(true);
     }
 
     const output = await textResponse.json();
+
     console.log(output);
     setOutput(output.stdout);
+
+    if (output.outcome !== 15) {
+      setErrorMsg("Something went wrong.");
+      setIsErrorVisible(true);
+    }
   };
+
+  useEffect(() => {
+    if (isErrorVisible) {
+      const timer = setTimeout(() => {
+        setIsErrorVisible(false);
+      }, 2000);  // Change this value to adjust the delay
+
+      return () => clearTimeout(timer);
+    }
+  }, [isErrorVisible]);
 
   return (
     <>
-      <div>
+      <div style={{paddingBottom: "5px"}}>
+        <p>{`Selected language is ${selectedLanguage}`}</p>
         <select value={selectedLanguage} default={selectedLanguage} onChange={dropDownChangeHandler}>
             <option value='c'>C</option>
             <option value='java'>Java</option>
             <option value='nodejs'>NodeJS</option>
             <option value='python3'>Python</option>
-            <option value='octave'>Octave</option>
+            <option value='php'>Php</option>
             <option value='cpp'>C++</option>
             <option value='pascal'>Pascal</option>
         </select>
@@ -115,13 +136,18 @@ function CodeEditor() {
           }}
         />
       </div>
-      <div>
+      <div style={{paddingTop: "7px"}}>
         <button onClick={submitHandler}>Run</button>
       </div>
       <div>
           <h3>Output: </h3>
           <p>{output}</p>
       </div>
+      {isErrorVisible && (
+        <div style={{ position: 'fixed', bottom: 10, left: 10, backgroundColor: 'red', color: 'white', padding: '5px' }}>
+          {errorMsg}
+        </div>
+      )}
     </>
   );
 }
