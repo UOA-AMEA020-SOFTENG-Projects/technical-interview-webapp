6dc376a73bfbab6c920070bfb8fe9c62e4d26cb5
IbrahimAnees <iane056@aucklanduni.ac.nz>
2023-07-07 17:26:25 +1200

    Implemented JWT

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,5 +1,12 @@
 import express from "express";
 import { createUser } from "../dao/userDAO.js";
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+import User from '../models/user.js';
+
+dotenv.config();
+const SECRET_KEY = process.env.SECRET_KEY;
 
 const userRouter = new express.Router();
 
@@ -10,6 +17,66 @@ userRouter.post("/user", async (req, res) => {
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
+  
 });
+
+
+userRouter.post('/user/register', async (req, res, next) => {
+  try {
+      const hashedPassword = await bcrypt.hash(req.body.password, 10);
+      const user = new User({ username: req.body.username, hashedPassword: hashedPassword });
+      await user.save();
+      res.status(201).send();
+  } catch (error) {
+      if (error.code === 11000) {
+          res.status(400).json({ message: "Username already in use" });
+      } else {
+          next(error);
+      }
+  }
+});
+
+userRouter.post('/user/login', async (req, res, next) => {
+  const user = await User.findOne({ username: req.body.username });
+  if (user == null) {
+      return res.status(400).send('Cannot find user');
+  }
+  try {
+      if (await bcrypt.compare(req.body.password, user.hashedPassword)) {
+          const accessToken = jwt.sign({username: user.username}, SECRET_KEY);
+          res.json({ accessToken: accessToken });
+      } else {
+          res.send('Not Allowed');
+      }
+  } catch (error) {
+      next(error);
+  }
+});
+
+userRouter.get('/user/profile', authenticateToken, async (req, res, next) => {
+try {
+  const user = await User.findOne({ username: req.user.username });
+  if (!user) {
+    return res.status(404).send('User not found');
+  }
+  const { hashedPassword, ...userData } = user._doc;
+  res.json(userData);
+} catch (error) {
+  next(error);
+}
+});
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, SECRET_KEY, (err, user) => {
+      if (err) return res.sendStatus(403);
+      req.user = user;
+      next();
+  })
+}
 
 export default userRouter;
