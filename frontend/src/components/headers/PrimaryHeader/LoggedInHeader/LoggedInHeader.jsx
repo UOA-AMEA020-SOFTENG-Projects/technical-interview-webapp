10a2b4aa7557f42266c1c23b566fd1b08e315f45
Jaskaran Sandhu <79944764+jsan956-ai@users.noreply.github.com>
2023-07-10 03:39:40 +1200

    sign in button on landing page navigation linked and the sign out functionality implemented

--- frontend/src/components/headers/PrimaryHeader/LoggedInHeader/LoggedInHeader.jsx
+++ frontend/src/components/headers/PrimaryHeader/LoggedInHeader/LoggedInHeader.jsx
@@ -1,8 +1,17 @@
 import React from "react"; 
+import { useNavigate } from "react-router-dom";
 import logo from "../../../../assets/primarylogo.png";
 import styles from "./LoggedInHeader.module.css";
 
 const LoggedInHeader = ({ username }) => {
+
+    const navigate = useNavigate();
+
+    const signoutHandler = () => {
+        localStorage.removeItem("authToken");
+        navigate("/");
+    }
+
     return (
         <header className={styles.header}>
             <div>
@@ -10,7 +19,7 @@ const LoggedInHeader = ({ username }) => {
             </div>
             <div className={styles.userInfo}>
                 <span className={styles.username}>Logged in as <b>{username}</b></span>
-                <button className={styles.signout}>Sign Out</button>
+                <button className={styles.signout} onClick={signoutHandler}>Sign Out</button>
             </div>
         </header>
     );
