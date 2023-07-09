dd40f567b3cd07b30968f4eb20dc4a42b01cd522
Jaskaran Sandhu <79944764+jsan956-ai@users.noreply.github.com>
2023-07-10 03:26:21 +1200

    error handling for sign up form as well as route protection implemented based on auth token

--- frontend/src/components/headers/PrimaryHeader/LoggedInHeader/LoggedInHeader.jsx
+++ frontend/src/components/headers/PrimaryHeader/LoggedInHeader/LoggedInHeader.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import logo from "../../../../assets/primarylogo.png";
 import styles from "./LoggedInHeader.module.css";
 
-const LoggedInHeader = () => {
+const LoggedInHeader = ({ username }) => {
     return (
         <header className={styles.header}>
             <div>
                 <img src={logo} alt="Logo" className={styles.logo}></img>
             </div>
             <div className={styles.userInfo}>
-                <span className={styles.username}>Logged in as <b>username</b></span>
+                <span className={styles.username}>Logged in as <b>{username}</b></span>
                 <button className={styles.signout}>Sign Out</button>
             </div>
         </header>
--- frontend/src/pages/HomeRootPage.jsx
+++ frontend/src/pages/HomeRootPage.jsx
@@ -1,17 +1,19 @@
 import React from "react";
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet, useLocation, useLoaderData, redirect, json } from "react-router-dom";
 import LoggedInHeader from "../components/headers/PrimaryHeader/LoggedInHeader/LoggedInHeader";
 
 const HomeRootPage = () => {
-
+  const data = useLoaderData();
   const location = useLocation();
   const route = location.pathname;
+
+  console.log(data, 10);
 
   if (route !== '/' && route !== '/login' && route !== '/signup'){
     
     return (
       <>  
-          <LoggedInHeader />
+          <LoggedInHeader username={data.username} />
           <main>
               <Outlet />
           </main>
@@ -29,3 +31,28 @@ const HomeRootPage = () => {
 };
 
 export default HomeRootPage;
+
+export const loader = async ({ request, params }) => {
+
+  const token = localStorage.getItem('authToken');
+
+  const response = await fetch('http://localhost:3000/user/profile', {
+    method: 'GET',
+    headers: {
+        'Authorization': 'Bearer ' + token
+    }
+  });
+
+  if (!response.ok) {
+     
+      if (response.status === 401 || response.status === 403){
+          return redirect("/");
+      } 
+
+      return json({ message: "User does not exist." }, { status: 500 });
+  } 
+
+  const user = await response.json();
+
+  return user;
+}
