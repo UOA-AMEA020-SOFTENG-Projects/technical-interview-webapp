7ae4761aa9f9bf2a853ab9ac223615877e380a4e
Jaskaran Sandhu <79944764+jsan956-ai@users.noreply.github.com>
2023-07-10 02:10:44 +1200

    fixed conditional rendering off the header

--- frontend/src/pages/HomeRootPage.jsx
+++ frontend/src/pages/HomeRootPage.jsx
@@ -1,7 +1,23 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
+import LoggedInHeader from "../components/headers/PrimaryHeader/LoggedInHeader/LoggedInHeader";
 
 const HomeRootPage = () => {
+
+  const location = useLocation();
+  const route = location.pathname;
+
+  if (route !== '/' && route !== '/login' && route !== '/signup'){
+    
+    return (
+      <>  
+          <LoggedInHeader />
+          <main>
+              <Outlet />
+          </main>
+      </>
+    );  
+  }
 
   return (
     <>
