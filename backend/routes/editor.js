e48d08f25afaeb5625b5437500a40e1814bc2499
IbrahimAnees <iane056@aucklanduni.ac.nz>
2023-07-09 23:42:37 +1200

    created clear-solution endpoint

--- backend/routes/editor.js
+++ backend/routes/editor.js
@@ -59,261 +59,9 @@ editorRouter.put("/editor/similarity/:problemId", async (req, res) => {
   }
 });
 
-// /*
-//   Endpoint #2: runs the code submitted by the user in the frontend on the jobe server
-//   and returns the output. The endpoint takes the language id as a query param and the code
-//   is submitted as part of the request body.
-// */
-// editorRouter.post(
-//   "/editor/:problemId/code",
-//   authenticateToken,
-//   async (req, res, next) => {
-//     try {
-//       const sourcecode = req.body.code;
-//       const selectedLanguage = req.query.language_id;
-//       const problemId = req.params.problemId;
-
-//       // Fetch problem from the database by its ID
-//       const problem = await Problem.findById(problemId);
-//       if (!problem) {
-//         return res
-//           .status(StatusCodes.NOT_FOUND)
-//           .json({ message: "Problem not found" });
-//       }
-
-//       // Fetch user and update or add solution to currentSolutions
-//       const user = await User.findOne({ username: req.user.username });
-//       if (!user) {
-//         return res
-//           .status(StatusCodes.NOT_FOUND)
-//           .json({ message: "User not found" });
-//       }
-
-//       // Check if user's currentSolutions contain an object with both problem id and language
-//       const userSolution = user.currentSolutions.find(
-//         (solution) =>
-//           solution.problem.toString() === problemId &&
-//           solution.language === selectedLanguage
-//       );
-
-//       // If solution exists, update it, otherwise add a new one
-//       if (userSolution) {
-//         userSolution.solution = sourcecode;
-//       } else {
-//         user.currentSolutions.push({
-//           problem: problemId,
-//           language: selectedLanguage,
-//           solution: sourcecode,
-//         });
-//       }
-
-//       // Save updated user document
-//       await user.save();
-
-//       console.log("-------------------------------------------");
-//       console.log("code: " + sourcecode, 67);
-//       console.log("selected language: " + selectedLanguage, 68);
-//       console.log("-------------------------------------------");
-
-//       let fileName = "";
-
-//       switch (selectedLanguage) {
-//         case "c":
-//           fileName = "output.c";
-//           break;
-//         case "java":
-//           fileName = "Output.java";
-//           break;
-//         case "nodejs":
-//           fileName = "output.js";
-//           break;
-//         case "python3":
-//           fileName = "output.py";
-//           break;
-//         case "cpp":
-//           fileName = "output.cpp";
-//           break;
-//         case "php":
-//           fileName = "output.php";
-//           break;
-//         default:
-//           throw new Error("Language id is invalid");
-//       }
-
-//       const codeData = {
-//         run_spec: {
-//           language_id: selectedLanguage,
-//           sourcefilename: fileName,
-//           sourcecode: sourcecode,
-//         },
-//       };
-
-//       const response = await axios.post(
-//         "http://localhost:4000/jobe/index.php/restapi/runs",
-//         codeData,
-//         {
-//           headers: {
-//             "Content-Type": "application/json",
-//           },
-//         }
-//       );
-
-//       if (response.status !== 200) {
-//         return res
-//           .status(StatusCodes.INTERNAL_SERVER_ERROR)
-//           .send({ message: "Error with Jobe server connection" });
-//       }
-
-//       if (response.data.outcome !== 15) {
-//         return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-//           error: response.data.cmpinfo,
-//           outcome: response.data.outcome,
-//         });
-//       }
-
-//       return res.status(StatusCodes.OK).json({ output: response.data.stdout });
-//     } catch (error) {
-//       return res
-//         .status(StatusCodes.INTERNAL_SERVER_ERROR)
-//         .send({ message: error.message });
-//     }
-//   }
-// );
-
-// /*
-//   Endpoint #3: fetches the test cases for the problem and runs them against the code and returns
-//   the outcome for each test case and whether the code passed each one or not.
-// */
-// editorRouter.post(
-//   "/editor/:problemId/testCase",
-//   authenticateToken,
-//   async (req, res, next) => {
-//     try {
-//       const sourcecode = req.body.code;
-//       const selectedLanguage = req.query.language_id;
-//       const problemId = req.params.problemId;
-
-//       // Fetch problem from the database by its ID
-//       const problem = await Problem.findById(problemId);
-//       if (!problem) {
-//         return res
-//           .status(StatusCodes.NOT_FOUND)
-//           .json({ message: "Problem not found" });
-//       }
-
-//       let fileName = "";
-
-//       switch (selectedLanguage) {
-//         case "c":
-//           fileName = "output.c";
-//           break;
-//         case "java":
-//           fileName = "Output.java";
-//           break;
-//         case "nodejs":
-//           fileName = "output.js";
-//           break;
-//         case "python3":
-//           fileName = "output.py";
-//           break;
-//         case "cpp":
-//           fileName = "output.cpp";
-//           break;
-//         case "php":
-//           fileName = "output.php";
-//           break;
-//         default:
-//           throw new Error("Language id is invalid");
-//       }
-
-//       // Fetch user and update or add solution to currentSolutions
-//       const user = await User.findOne({ username: req.user.username });
-//       if (!user) {
-//         return res
-//           .status(StatusCodes.NOT_FOUND)
-//           .json({ message: "User not found" });
-//       }
-
-//       // Check if user's currentSolutions contain an object with both problem id and language
-//       const userSolution = user.currentSolutions.find(
-//         (solution) =>
-//           solution.problem.toString() === problemId &&
-//           solution.language === selectedLanguage
-//       );
-
-//       // If solution exists, update it, otherwise add a new one
-//       if (userSolution) {
-//         userSolution.solution = sourcecode;
-//       } else {
-//         user.currentSolutions.push({
-//           problem: problemId,
-//           language: selectedLanguage,
-//           solution: sourcecode,
-//         });
-//       }
-
-//       // Save updated user document
-//       await user.save();
-
-//       let testResults = [];
-
-//       for (let i = 0; i < problem.testCases.length; i++) {
-//         const testcase = problem.testCases[i];
-
-//         const codeData = {
-//           run_spec: {
-//             language_id: selectedLanguage,
-//             sourcefilename: fileName,
-//             sourcecode: sourcecode,
-//             input: testcase.input,
-//           },
-//         };
-
-//         const response = await axios.post(
-//           "http://localhost:4000/jobe/index.php/restapi/runs",
-//           codeData,
-//           {
-//             headers: {
-//               "Content-Type": "application/json",
-//             },
-//           }
-//         );
-
-//         console.log("----------------------------------");
-//         console.log(response.data);
-//         console.log("----------------------------------");
-
-//         if (response.status !== 200) {
-//           return res
-//             .status(StatusCodes.INTERNAL_SERVER_ERROR)
-//             .send({ message: "Error with Jobe server connection" });
-//         }
-
-//         if (response.data.outcome !== 15) {
-//           return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-//             error: response.data.cmpinfo,
-//             outcome: response.data.outcome,
-//           });
-//         }
-
-//         // Check if the output matches the expected output
-//         if (response.data.stdout.trim() === testcase.output.trim()) {
-//           testResults.push({ testcase: i + 1, passed: true });
-//         } else {
-//           testResults.push({ testcase: i + 1, passed: false });
-//         }
-//       }
-
-//       return res.status(StatusCodes.OK).json({ testResults });
-//     } catch (error) {
-//       return res
-//         .status(StatusCodes.INTERNAL_SERVER_ERROR)
-//         .send({ message: error.message });
-//     }
-//   }
-// );
-
-
+/**
+ * Save solution
+ */
 editorRouter.post(
   "/editor/:problemId/saveSolution",
   authenticateToken,
@@ -323,7 +71,6 @@ editorRouter.post(
       const selectedLanguage = req.query.language_id;
       const problemId = req.params.problemId;
 
-      // Check if the required parameters are provided
       if (!sourcecode) {
         return res
           .status(StatusCodes.BAD_REQUEST)
@@ -342,7 +89,6 @@ editorRouter.post(
           .json({ message: "Problem ID is not valid" });
       }
 
-      // Fetch problem from the database by its ID
       const problem = await Problem.findById(problemId);
       if (!problem) {
         return res
@@ -350,7 +96,6 @@ editorRouter.post(
           .json({ message: "Problem not found" });
       }
 
-      // Fetch user and update or add solution to currentSolutions
       const user = await User.findOne({ username: req.user.username });
       if (!user) {
         return res
@@ -358,14 +103,12 @@ editorRouter.post(
           .json({ message: "User not found" });
       }
 
-      // Check if user's currentSolutions contain an object with both problem id and language
       const userSolution = user.currentSolutions.find(
         (solution) =>
           solution.problem.toString() === problemId &&
           solution.language === selectedLanguage
       );
 
-      // If solution exists, update it, otherwise add a new one
       if (userSolution) {
         userSolution.solution = sourcecode;
       } else {
@@ -376,12 +119,60 @@ editorRouter.post(
         });
       }
 
-      // Save updated user document
       await user.save();
 
       return res
         .status(StatusCodes.OK)
         .send({ message: "Solution saved successfully" });
+    } catch (error) {
+      return res
+        .status(StatusCodes.INTERNAL_SERVER_ERROR)
+        .send({ message: error.message });
+    }
+  }
+);
+
+/**
+ * Clear solution
+ */
+editorRouter.delete(
+  '/editor/:problemId/clearSolution',
+  authenticateToken,
+  async (req, res, next) => {
+    try {
+      const selectedLanguage = req.query.language_id;
+      const problemId = req.params.problemId;
+
+      if (!mongoose.Types.ObjectId.isValid(problemId)) {
+        return res
+          .status(StatusCodes.BAD_REQUEST)
+          .json({ message: "Problem ID is not valid" });
+      }
+
+      const user = await User.findOne({ username: req.user.username });
+      if (!user) {
+        return res
+          .status(StatusCodes.NOT_FOUND)
+          .json({ message: "User not found" });
+      }
+
+      // Get the index of the solution to delete
+      const solutionIndex = user.currentSolutions.findIndex(
+        (solution) =>
+          solution.problem.toString() === problemId &&
+          solution.language === selectedLanguage
+      );
+
+      if (solutionIndex !== -1) {
+        user.currentSolutions.splice(solutionIndex, 1);
+        await user.save();
+        return res.status(StatusCodes.OK).send({ message: "Solution cleared successfully" });
+      } else {
+        return res
+          .status(StatusCodes.NOT_FOUND)
+          .json({ message: "Solution not found" });
+      }
+
     } catch (error) {
       return res
         .status(StatusCodes.INTERNAL_SERVER_ERROR)
