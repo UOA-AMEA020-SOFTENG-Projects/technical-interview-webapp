a9f620d0eb704af77a9055d971e770028590978b
IbrahimAnees <iane056@aucklanduni.ac.nz>
2023-07-09 12:34:56 +1200

    fixed user endpoints

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -4,60 +4,65 @@ import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 import User from '../models/user.js';
+import { authenticateToken } from "../middleware/authenticator.js";
 
 dotenv.config();
 const SECRET_KEY = process.env.SECRET_KEY;
 
 const userRouter = new express.Router();
 
-userRouter.post("/user", async (req, res) => {
-  try {
-    const newUser = await createUser(req.body);
-    res.json(newUser);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-  
-});
-
-
+/**
+ * POST /user/register
+ * Register a new user
+ */
 userRouter.post('/user/register', async (req, res, next) => {
   try {
-      const hashedPassword = await bcrypt.hash(req.body.password, 10);
-      const user = new User({ username: req.body.username, hashedPassword: hashedPassword });
-      await user.save();
-      res.status(201).send();
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const user = new User({ username: req.body.username, hashedPassword: hashedPassword });
+    await user.save();
+
+    const accessToken = jwt.sign({username: user.username}, SECRET_KEY);
+
+    res.status(201).json({ message: "User successfully registered", accessToken: accessToken });
   } catch (error) {
-      if (error.code === 11000) {
-          res.status(400).json({ message: "Username already in use" });
-      } else {
-          next(error);
-      }
+    if (error.code === 11000) {
+        res.status(400).json({ message: "Username already in use" });
+    } else {
+        next(error);
+    }
   }
 });
 
+/**
+ * POST /user/login
+ * Login to existing user
+ */
 userRouter.post('/user/login', async (req, res, next) => {
   const user = await User.findOne({ username: req.body.username });
   if (user == null) {
-      return res.status(400).send('Cannot find user');
+      return res.status(400).json({ message: "Cannot find user" });
   }
   try {
       if (await bcrypt.compare(req.body.password, user.hashedPassword)) {
           const accessToken = jwt.sign({username: user.username}, SECRET_KEY);
           res.json({ accessToken: accessToken });
       } else {
-          res.send('Not Allowed');
+          res.status(401).json({ message: "Unauthorized: Invalid password" });
       }
   } catch (error) {
       next(error);
   }
 });
 
+/**
+ * GET /user/profile
+ * Get currently logged in user's profile
+ */
 userRouter.get('/user/profile', authenticateToken, async (req, res, next) => {
 try {
   const user = await User.findOne({ username: req.user.username });
   if (!user) {
-    return res.status(404).send('User not found');
+    return res.status(404).json({ message: "Cannot find user" });
   }
   const { hashedPassword, ...userData } = user._doc;
   res.json(userData);
@@ -66,17 +71,6 @@ try {
 }
 });
 
-function authenticateToken(req, res, next) {
-  const authHeader = req.headers['authorization'];
-  const token = authHeader && authHeader.split(' ')[1];
 
-  if (token == null) return res.sendStatus(401);
-
-  jwt.verify(token, SECRET_KEY, (err, user) => {
-      if (err) return res.sendStatus(403);
-      req.user = user;
-      next();
-  })
-}
 
 export default userRouter;
