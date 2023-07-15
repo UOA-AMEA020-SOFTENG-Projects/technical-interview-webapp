909e5f1e0e1d505d0f27d1b8338d19e2ab786e7e
Jaskaran Sandhu <79944764+jsan956-ai@users.noreply.github.com>
2023-07-16 10:55:48 +1200

    get progress per topic endpoint consumed

--- frontend/src/components/dashboard/TopicsList/TopicsList.jsx
+++ frontend/src/components/dashboard/TopicsList/TopicsList.jsx
@@ -1,10 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import { Container, Row, Col, Card, Modal, Button } from 'react-bootstrap';
 
 const TopicsList = ({ topics }) => {
   const [show, setShow] = useState(false);
+  const token = localStorage.getItem("authToken");
   const [currentTopic, setCurrentTopic] = useState(null);
+  const [topicsProgress, setTopicsProgress] = useState({});  // Add this state to store topic progress
 
   const navigate = useNavigate();
 
@@ -17,6 +19,25 @@ const TopicsList = ({ topics }) => {
   const navigateContentHandler = (topicId, title) => {
     navigate(`/home/content/${topicId}?title=${title}`); 
   }
+
+  useEffect(() => {
+    const fetchProgress = async (topicId) => {
+      const response = await fetch(`http://localhost:3000/topic/${topicId}/progress`, {
+        headers: {
+          'Authorization': `Bearer ${token}`
+        }
+      });
+      const data = await response.json();
+      console.log(data.progress,31);
+      return data.progress;
+    };
+
+    topics.forEach(async (topic) => {
+      const progress = await fetchProgress(topic._id);
+      setTopicsProgress((prev) => ({ ...prev, [topic._id]: progress }));
+    });
+  }, [topics]);
+
 
   return (
     <Container className="d-flex justify-content-center align-items-center vh-100">
@@ -35,6 +56,7 @@ const TopicsList = ({ topics }) => {
               <Card.Body>
                 <Card.Title>{topic.title}</Card.Title>
                 <Card.Text>{`Length: ${topic.length}`}</Card.Text>
+                <Card.Text>{`Progress: ${topicsProgress[topic._id]}%`}</Card.Text> {/* Display the progress */}
               </Card.Body>
             </Card>
           </Col>
