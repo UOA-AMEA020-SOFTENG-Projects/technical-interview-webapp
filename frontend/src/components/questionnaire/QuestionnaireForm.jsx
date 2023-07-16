4d9b91f20f3ffd32990b500e3189ebc5ec6b429a
Jaskaran Sandhu <79944764+jsan956-ai@users.noreply.github.com>
2023-07-17 11:48:22 +1200

    loader implemented for questionnaire as well as skeleton code for components

---
+++ frontend/src/components/questionnaire/QuestionnaireForm.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "./QuestionnaireForm.module.css";
+
+const QuestionnaireForm = ({ questions }) => {
+  
+  console.log(questions, 6);
+
+  return (
+    <div>
+        <h2>Questionnaire</h2>
+    </div>
+  );
+};
+
+export default QuestionnaireForm;
---
+++ frontend/src/pages/QuestionnairePage.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { useLoaderData, redirect, json } from 'react-router-dom';
+import QuestionnaireForm from "../components/questionnaire/QuestionnaireForm";
+
+const QuestionnairePage = () => {
+  
+    const questions = useLoaderData();
+
+    return (
+        <QuestionnaireForm questions={questions}/>
+    );
+};
+
+export default QuestionnairePage;
+
+export const loader = async ({ request, params }) => {
+
+
+    const response = await fetch('http://localhost:3000/questions');
+  
+    if (!response.ok) {
+        const err = await response.json();
+  
+        return json({ message: err.message }, { status: 500 });
+    } else {
+        const data = await response.json();
+  
+        console.log("questions: " + data, 25);
+  
+        return data;
+    }
+  }
