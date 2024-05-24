b887f5fad5edec10ec3a8fb6e14a8f93b8eed7df
niraj <62448681+nirajpatilcs@users.noreply.github.com>
2024-05-24 19:25:06 +1200

    added mongo

--- backend-ts/src/index.ts
+++ backend-ts/src/index.ts
@@ -1,15 +1,25 @@
 import express, { Express, Request, Response } from "express";
+import cors from "cors";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 dotenv.config();
 
-const app: Express = express();
 const port = process.env.PORT || 3000;
+const MONGO_URI = process.env.MONGO_URI!;
+
+const app: Express = express();
+
+app.use(cors());
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
+
+mongoose.connect(MONGO_URI).then(() => {
+  console.log("Connected to MongoDB");
+});
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
-});
-
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
 });
