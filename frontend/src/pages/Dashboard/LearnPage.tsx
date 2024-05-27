009288e264afa11f19c8b08a36e8b32a51580dc8
niraj <62448681+nirajpatilcs@users.noreply.github.com>
2024-05-27 14:06:37 +1200

    addded content on learn page

--- frontend/src/pages/Dashboard/LearnPage.tsx
+++ frontend/src/pages/Dashboard/LearnPage.tsx
@@ -1,12 +1,30 @@
 import { Topic } from "@/types";
-import React, { useMemo } from "react";
+import React, { useCallback, useMemo, useState } from "react";
 import TopicElement from "../../components/dashboard/Topic";
+import TopicContent from "../../components/content/TopicContent/TopicContent";
+import { IconButton, Typography } from "@mui/material";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 
 interface Props {
   topics: Topic[];
 }
 
 const LearnPage = ({ topics }: Props) => {
+  const [showContent, setShowContent] = useState(false);
+  const [currentTopic, setCurrentTopic] = useState({
+    contentId: "",
+    topicName: "",
+  });
+
+  const handleTopicCardClick = (topic: Topic) => {
+    setCurrentTopic({ contentId: topic.content, topicName: topic.title });
+    setShowContent(true);
+  };
+
+  const toggleShowContent = useCallback(() => {
+    setShowContent((prev) => !prev);
+  }, []);
+
   const topicListItems = useMemo(() => {
     return topics.map((topic, index) => (
       <TopicElement
@@ -14,12 +32,26 @@ const LearnPage = ({ topics }: Props) => {
         delay={index * 0.02}
         key={index}
         cardcolor="#f5f5f5"
-        handleClick={() => console.log("navigate to content")}
+        handleClick={handleTopicCardClick}
       />
     ));
   }, [topics]);
 
-  return <div className="learn-page-cont">{topicListItems}</div>;
+  return (
+    <div className="learn-page-cont">
+      {showContent ? (
+        <>
+          <IconButton size="small" onClick={toggleShowContent}>
+            <ArrowBackIosIcon fontSize="small" />
+          </IconButton>
+          <Typography variant="h4">{currentTopic.topicName}</Typography>
+          <TopicContent contentId={currentTopic.contentId} />
+        </>
+      ) : (
+        topicListItems
+      )}
+    </div>
+  );
 };
 
 export default LearnPage;
--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -8,4 +8,12 @@ export interface Topic {
   _id: string;
   title: string;
   problems: Problem[];
+  content: string;
+}
+
+export interface Content {
+  primaryDescription: string;
+  secondaryDescription: string;
+  videoURL: string;
+  code: string;
 }
