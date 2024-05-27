c29cfb0ee9f1d48b80007918979ec92ad0e4f335
niraj <62448681+nirajpatilcs@users.noreply.github.com>
2024-05-27 12:55:59 +1200

    added logout button and help button

--- frontend/src/components/headers/PrimaryHeader/LoggedInHeader/LoggedInHeader.tsx
+++ frontend/src/components/headers/PrimaryHeader/LoggedInHeader/LoggedInHeader.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { useNavigate, Link } from "react-router-dom";
 import styles from "./LoggedInHeader.module.css";
-import { Avatar, IconButton, Typography } from "@mui/material";
-import NotificationsNoneRoundedIcon from "@mui/icons-material/NotificationsNoneRounded";
+import { Avatar, IconButton, Tooltip, Typography } from "@mui/material";
 import HelpOutlineRoundedIcon from "@mui/icons-material/HelpOutlineRounded";
+import LogoutRoundedIcon from "@mui/icons-material/LogoutRounded";
 
 interface Props {
   username: string;
@@ -21,27 +21,31 @@ const LoggedInHeader = ({ username }: Props) => {
     <header className={styles.header}>
       <div className={styles.logoSearch}>
         <div>
-          <Link to={`/home/dashboard`} style={{ textDecoration: "none" }}>
-            <Typography style={{ color: "black" }} variant="body1">
-              <span style={{ fontWeight: "bold" }}>ALGO</span> CHAMP
-            </Typography>
-          </Link>
+          <Tooltip title="Go Home">
+            <Link to={`/home/dashboard`} style={{ textDecoration: "none" }}>
+              <Typography style={{ color: "black" }} variant="body1">
+                <span style={{ fontWeight: "bold" }}>ALGO</span> CHAMP
+              </Typography>
+            </Link>
+          </Tooltip>
         </div>
       </div>
 
       <div className={styles.userInfo}>
-        <IconButton aria-label="help">
-          <HelpOutlineRoundedIcon />
-        </IconButton>
-        <IconButton aria-label="notification">
-          <NotificationsNoneRoundedIcon />
-        </IconButton>
+        <Tooltip title="Help">
+          <IconButton aria-label="help">
+            <HelpOutlineRoundedIcon />
+          </IconButton>
+        </Tooltip>
+        <Tooltip title="Logout">
+          <IconButton aria-label="logout" onClick={signoutHandler}>
+            <LogoutRoundedIcon />
+          </IconButton>
+        </Tooltip>
         <Typography fontSize={"0.75rem"} variant="overline">
           {username}
         </Typography>
-        <IconButton aria-label="sign-out" onClick={signoutHandler}>
-          <Avatar>A</Avatar>
-        </IconButton>
+        <Avatar>A</Avatar>
       </div>
     </header>
   );
--- frontend/src/components/headers/SecondaryHeader/SecondaryHeader.jsx
+++ frontend/src/components/headers/SecondaryHeader/SecondaryHeader.jsx
@@ -1,20 +1,24 @@
-import React from 'react'; 
-import { useNavigate } from 'react-router-dom';
-import styles from './SecondaryHeader.module.css'; 
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import styles from "./SecondaryHeader.module.css";
 
 const SecondaryHeader = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const signinHandler = () => {
-        navigate("/login");
-    }
+  const signinHandler = () => {
+    navigate("/login");
+  };
 
-    return (
-        <div className={styles.container}>
-            <p className={styles.message}>Be confident in your technical interview.</p>
-            <button className={styles.button} onClick={signinHandler}>Sign In</button>
-        </div>
-    );
-}
+  return (
+    <div className={styles.container}>
+      <p className={styles.message}>
+        Be confident in your technical interview.
+      </p>
+      <button className={styles.button} onClick={signinHandler}>
+        Sign In
+      </button>
+    </div>
+  );
+};
 
 export default SecondaryHeader;
